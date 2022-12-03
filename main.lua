b0d14b0251e57a7befc58ab21738b3d01d6fc2da
Alan1321 <alansubedi123@gmail.com>
2022-12-03 11:06:24 -0600

    Init Commit

---
+++ main.lua
@@ -0,0 +1,10 @@
+--Alan Subedi
+
+------------------------------------------
+widget = require( "widget" )
+local composer = require("composer")
+
+display.setStatusBar(display.HiddenStatusBar)
+
+composer.gotoScene("scene1")
+----------------------------------------------
